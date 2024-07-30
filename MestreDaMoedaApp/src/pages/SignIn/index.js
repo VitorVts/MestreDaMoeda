ca4245024945250d858bdd4e1bab8ceecaf0d0cb
vitorvts <vitorgguimaraes56@gmail.com>
2024-07-30 17:21:49 -0300

    Feat: Criado formulário para login junto ao botão de acessar e um link para a tela de cadastro

--- MestreDaMoedaApp/src/pages/SignIn/index.js
+++ MestreDaMoedaApp/src/pages/SignIn/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { Platform } from "react-native";
+import { Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText } from "./styles";
+import {} from '@react-navigation/stack';
+import { useNavigation } from "@react-navigation/native";
 
-export default function SignIn(){
-  return(
-    <View>
-      <Text>TELA LOGIN</Text>
-    </View>
-  )
+
+
+export default function SignIn() {
+  const navigation = useNavigation();
+
+
+  return (
+    <Background>
+      <Container
+      behavior ={Platform.OS === 'ios' ?'padding':''}
+      enabled
+      >
+        <Logo source={require("../../assets/Logo.png")} />
+
+        <AreaInput>
+          <Input placeholder="Seu Email" />
+        </AreaInput>
+         <AreaInput>
+          <Input placeholder="Sua Senha" />
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+        <Link onPress={()=>navigation.navigate("SignUp")}>
+        <LinkText>Criar Uma Conta</LinkText>
+        </Link>
+
+
+      </Container>
+    </Background>
+  );
 }
