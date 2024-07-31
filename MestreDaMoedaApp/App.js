ddc3aa9932d378915025ac0fdb38d773d51429d8
vitorvts <vitorgguimaraes56@gmail.com>
2024-07-31 14:56:39 -0300

    feat:Cria as rotas do drawerNavigator  após login e cria a página home

--- MestreDaMoedaApp/App.js
+++ MestreDaMoedaApp/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import 'react-native-gesture-handler';
+import React from "react";
+import { StatusBar } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 
-import Routes from './src/routes/index';
+import Routes from "./src/routes/index";
 
-export default function App(){
-  return(
+import AuthProvider from "./src/contexts/auth";
+
+export default function App() {
+  return (
     <NavigationContainer>
-      <StatusBar backgroundColor="#F0F4FF" barStyle="dark-content" />
-      <Routes/>
+      <AuthProvider>
+        <StatusBar backgroundColor="#F0F4FF" barStyle="dark-content" />
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
-  )
+  );
 }
--- MestreDaMoedaApp/src/pages/SignUp/index.js
+++ MestreDaMoedaApp/src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Platform } from "react-native";
+import React, { useContext, useState } from "react";
+import { Platform, ActivityIndicator } from "react-native";
 
 import {
   Background,
@@ -10,24 +10,54 @@ import {
   SubmitText,
 } from "../SignIn/styles";
 
+import { AuthContext } from "../../contexts/auth";
+
 export default function SignUp() {
+  const { signUp, loadingAuth } = useContext(AuthContext);
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp() {
+    if (nome === "" || email === "" || password === "") return;
+
+    signUp(email, password, nome);
+  }
+
   return (
     <Background>
       <Container behavior={Platform.OS === "ios" ? "padding" : ""} enabled>
         <AreaInput>
-          <Input placeholder="Nome" />
+          <Input
+            placeholder="Nome"
+            value={nome}
+            onChangeText={(text) => setNome(text)}
+          />
         </AreaInput>
 
         <AreaInput>
-          <Input placeholder="Email" />
+          <Input
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
         </AreaInput>
 
         <AreaInput>
-          <Input placeholder="Sua senha" />
+          <Input
+            placeholder="Senha"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+          />
         </AreaInput>
 
-        <SubmitButton>
-          <SubmitText>Cadastrar</SubmitText>
+        <SubmitButton onPress={handleSignUp}>
+          {loadingAuth ? (
+            <ActivityIndicator size={20} color="#fff" />
+          ) : (
+            <SubmitText>Cadastrar</SubmitText>
+          )}
         </SubmitButton>
       </Container>
     </Background>
