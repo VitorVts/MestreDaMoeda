0beac4233647824b2a5c61305a6584beecc4c4c0
vitorvts <vitorgguimaraes56@gmail.com>
2024-07-31 10:36:46 -0300

    feat:Criado formulario e botão de envio na tela de cadastro

--- MestreDaMoedaApp/src/pages/SignUp/index.js
+++ MestreDaMoedaApp/src/pages/SignUp/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { Platform } from "react-native";
 
-export default function SignUp(){
-  return(
-    <View>
-      <Text>TELA CADASTRO</Text>
-    </View>
-  )
+import {
+  Background,
+  Container,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+} from "../SignIn/styles";
+
+export default function SignUp() {
+  return (
+    <Background>
+      <Container behavior={Platform.OS === "ios" ? "padding" : ""} enabled>
+        <AreaInput>
+          <Input placeholder="Nome" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Email" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Sua senha" />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
